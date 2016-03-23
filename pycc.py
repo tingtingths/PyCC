81185a2f07f6d4334c358eb399dd497eacc9bcff
Tony Ting <k999hoshing@gmail.com>
2016-03-23 12:03:53 +0800

    Add convert using string from argument. Convert tab to spaces.

--- pycc.py
+++ pycc.py
@@ -8,80 +8,91 @@ from collections import deque
 
 
 def detect(bytes):
-	encoding_swap = {"big5":"cp950", "gb2312":"gbk"}
-	codec = chardet.detect(bytes)["encoding"]
-	if codec == None:
-		return None
-	return codec if codec.lower() not in encoding_swap else encoding_swap[codec.lower()]
+    encoding_swap = {"big5":"cp950", "gb2312":"gbk"}
+    codec = chardet.detect(bytes)["encoding"]
+    if codec == None:
+        return None
+    return codec if codec.lower() not in encoding_swap else encoding_swap[codec.lower()]
 
 
 def convert(s, d):
-	new_s = ""
+    new_s = ""
 
-	for c in s:
-		if c in d:
-			c = d[c]
-		new_s += str(c)
+    for c in s:
+        if c in d:
+            c = d[c]
+        new_s += str(c)
 
-	return new_s
+    return new_s
 
 
 def construct_dict(filename):
-	d = {}
+    d = {}
 
-	with open(filename, "r", encoding="utf8") as f:
-		for line in f:
-			lst = line.split("-")
-			d[lst[0]] = lst[1].rstrip().split(".")[0]
+    with open(filename, "r", encoding="utf8") as f:
+        for line in f:
+            lst = line.split("-")
+            d[lst[0]] = lst[1].rstrip().split(".")[0]
 
-	return d
+    return d
 
 
 def convert_files(d, files):
-	for f in files:
-		org_f = open(f, "rb")
-		org_filename = os.path.basename(org_f.name)
-		org_dirname = os.path.dirname(os.path.abspath(org_f.name))
-		codec = detect(org_f.read())
-		print(f)
-		if codec == None:
-			codec = input("Error detecting encoding, set encoding to >")
-			print("    Selected encoding: " + codec)
-		else:
-			print("    Suggested encoding: " + codec)
-		s = convert(open(f, encoding=codec).read(), d)
-		open(org_dirname + os.path.sep + "_" + org_filename,
-			"w", encoding="utf8").write(s)
+    for f in files:
+        org_f = open(f, "rb")
+        org_filename = os.path.basename(org_f.name)
+        org_dirname = os.path.dirname(os.path.abspath(org_f.name))
+        codec = detect(org_f.read())
+        print(f)
+        if codec == None:
+            codec = input("Error detecting encoding, set encoding to >")
+            print("    Selected encoding: " + codec)
+        else:
+            print("    Suggested encoding: " + codec)
+        s = convert(open(f, encoding=codec).read(), d)
+        open(org_dirname + os.path.sep + "_" + org_filename,
+            "w", encoding="utf8").write(s)
 
 
 def print_help():
-	print("Usage: " + os.path.basename(__file__) + " <command> [-g | -glob] <input_files | glob_pattern>")
-	print("  command:")
-	print("    -t2s Traditional chinese to Simplified chinese")
-	print("    -s2t Simplified chinese to Traditional chinese")
-	print("    -i Print suggested encoding only")
-	print("  -g, -glob Select files with glob pattern")
+    print("Usage: " + os.path.basename(__file__) + " <command> [option] <input_file | glob_pattern | string>...")
+    print("  command:")
+    print("    -t2s         Traditional chinese to Simplified chinese")
+    print("    -s2t         Simplified chinese to Traditional chinese")
+    print("    -i           Only print suggested encoding for file(s)")
+    print("  option:")
+    print("    -g, -glob    Select file(s) with glob pattern")
+    print("    -s           Convert using string(s) from argument")
 
 if __name__ == "__main__":
-	args = deque(sys.argv[1:])
+    args = deque(sys.argv[1:])
 
-	if len(sys.argv) < 2 or "-h" in args or "-help" in args:
-		print_help()
-	else:
-		try:
-			option = args.popleft()[1:]
-			if "-g" == args[0] or "-glob" == args[0]:
-				args.popleft()
-				files = glob.glob(args.popleft())
-			else:
-				files = args
-			cd = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "dict" + os.path.sep
-			if option == "i":
-				for f in files:
-					print("Suggested encoding:" + detect(open(f, "rb").read()))
-			elif option == "t2s" or option == "s2t":
-				convert_files(construct_dict(cd + option), files)
-			else:
-				print_help()
-		except Exception as e:
-			print(e)
+    if len(sys.argv) < 2 or "-h" in args or "-help" in args:
+        print_help()
+    else:
+        try:
+            command = args.popleft()[1:]
+            input_string = False
+            if "-g" == args[0] or "-glob" == args[0]:
+                args.popleft()
+                input = glob.glob(args.popleft())
+            elif "-s" == args[0]:
+                args.popleft()
+                input_string = True
+                input = args
+            else:
+                input = args
+            cd = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "dict" + os.path.sep
+            if command == "i":
+                for f in input:
+                    print("Suggested encoding:" + detect(open(f, "rb").read()))
+            elif command == "t2s" or command == "s2t":
+                if input_string:
+                    for s in args:
+                        print(convert(s, construct_dict(cd + command)))
+                else:
+                    convert_files(construct_dict(cd + command), input)
+            else:
+                print_help()
+        except Exception as e:
+            print(e)
